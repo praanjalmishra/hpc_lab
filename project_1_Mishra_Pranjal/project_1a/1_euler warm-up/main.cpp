2b883f78aff71eb2fd7b8a6d7564f2668c9830c6
Pranjal Mishra <39010495+praanjalmishra@users.noreply.github.com>
2024-03-16 23:33:20 +0100

    ADD proj1

---
+++ project_1_Mishra_Pranjal/project_1a/1_euler warm-up/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <unistd.h>
+#include <limits.h>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    // print host name
+    char hostname[HOST_NAME_MAX];
+    gethostname(hostname, HOST_NAME_MAX);
+    std::cout << "Host: " << hostname << std::endl;
+    return 0;
+}
---
+++ project_1_Mishra_Pranjal/project_1b/ 4_mat_mul_optimization/output/dgemm-blocked.c
@@ -0,0 +1,32 @@
+const char *dgemm_desc = "Blocked dgemm.";
+
+#pragma GCC optimize ("-O3,-ffast-math")
+
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+/* This routine performs a dgemm operation
+ *
+ *  C := C + A * B
+ *
+ * where A, B, and C are lda-by-lda matrices stored in column-major format.
+ * On exit, A and B maintain their input values.
+ */
+void square_dgemm(int n, double *A, double *B, double *C) {
+  // blocked matrix multiplication
+  int block_size = 256;
+  for (int i = 0; i < n; i += block_size) {
+    for (int j = 0; j < n; j += block_size) {
+      for (int k = 0; k < n; k += block_size) {
+        // block matrix multiplication
+        for (int i1 = i; i1 < min(i + block_size, n); i1++) {
+          for (int j1 = j; j1 < min(j + block_size, n); j1++) {
+            double b = B[i1 + j1 * n];
+            for (int k1 = k; k1 < min(k + block_size, n); k1++) {
+              C[k1 + j1 * n] += A[k1 + i1 * n] * b;
+            }
+          }
+        }
+      }
+    }
+  }
+}
